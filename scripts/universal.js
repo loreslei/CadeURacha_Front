29b8763b0f349bb8e0203cce58225abb5bdec572
Lorenna Aguiar Nunes <133262137+loreslei@users.noreply.github.com>
2025-01-30 09:40:08 -0300

    tela de jogos finalizada

--- scripts/universal.js
+++ scripts/universal.js
@@ -13,3 +13,19 @@ document.addEventListener("click", (event) => {
     });
   }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll(".arrow").forEach(arrow => {
+      arrow.addEventListener("click", function () {
+          let agrupador = this.closest("#Agrupador"); // Encontra o agrupador correto
+          let container = agrupador.querySelector(".container .row"); // Seleciona a div correta
+          let scrollAmount = 300; // Distância do scroll
+
+          if (this.classList.contains("bx-chevron-left")) {
+              container.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+          } else {
+              container.scrollBy({ left: scrollAmount, behavior: "smooth" });
+          }
+      });
+  });
+});
